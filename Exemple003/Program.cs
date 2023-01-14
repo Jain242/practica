df1f8d2c622cf141f2ffad8be85d9f0dcd69d789
Egor <agent-nino@bk.ru>
2023-01-14 21:19:37 +0300

    Random

--- Exemple003/Program.cs
+++ Exemple003/Program.cs
@@ -1,5 +1,7 @@
 ﻿
-int number1 = 3;
-int number2 = 5;
+int number1 = new Random().Next(1,10);
+Console.WriteLine(number1);
+int number2 = new Random().Next(1,10);
 int resault = number1 + number2;
+Console.WriteLine(number2);
 Console.WriteLine(resault);
