daeff776130491039f72bb330d3b41ebf348a4cf
Egor <agent-nino@bk.ru>
2023-01-15 13:47:50 +0300

    Example011

---
+++ Example008/Program.cs
@@ -0,0 +1,26 @@
+﻿int Max(int arg1, int arg2, int arg3){
+    int result = arg1;
+    if (result<arg2) result = arg2;
+    if (result<arg3) result = arg3;
+     return result;
+}
+
+int a1 = 23;
+int b1 = 34;
+int c1 = 43;
+int a2 = 42;
+int b2 = 31;
+int c2 = 899;
+int a3 = 27;
+int b3 = 32;
+int c3 = 90;
+
+
+int max1 = Max(a1,b1,c1);
+int max2 = Max(a2,b2,c2);
+int max3 = Max(a3,b3,c3);
+int max = Max(max1,max2,max3);
+
+
+
+System.Console.WriteLine(max);
---
+++ Example010/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {13,4,14,65,4,23,13,34};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+while (index < n )
+{
+    if (array[index] == find){
+        System.Console.WriteLine(index);
+    break;
+    }
+    index++;
+}
